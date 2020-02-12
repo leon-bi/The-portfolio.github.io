0fd16312e8795c3bf7eba71eab3584a5763a0fd9
bchizi <leonbichanga@gmail.com>
2020-02-12 08:33:29 +0300

    tried some fixing

--- js/script.js
+++ js/script.js
@@ -26,3 +26,25 @@ window.onclick=function(event){
         nav.classList.remove("nav-is-visible");
     }
 }
+
+$(document).ready(function() {
+    $('#lightgallery').lightGallery({
+      pager: true
+    });
+  });
+
+  $(function(){
+    //WOW plugin init
+    new WOW().init();
+
+    //parallax effect for banner
+    (function() {
+        var posY;
+        var image = document.getElementById('parallax');;
+        function paralax() {
+            posY = window.pageYOffset;
+            image.style.top = posY * 0.9 + 'px';
+        }
+        window.addEventListener('scroll', paralax);
+    })();
+});
